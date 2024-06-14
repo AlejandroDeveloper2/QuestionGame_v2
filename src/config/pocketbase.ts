bb6caf1fad42f81677777b80f368b76f5094a3aa
AlejandroDeveloper2 <90485793+AlejandroDeveloper2@users.noreply.github.com>
2024-06-13 20:52:24 -0500

    Cambio

--- src/config/pocketbase.ts
+++ src/config/pocketbase.ts
@@ -1,6 +1,6 @@
 import PocketBase from "pocketbase";
 
-const API_URL = import.meta.env.VITE_POCKETBASE_URL_LOCAL;
+const API_URL = import.meta.env.VITE_POCKETBASE_URL_PRODUCTION;
 
 export const apiUrl = API_URL;
 const client = new PocketBase(apiUrl);
